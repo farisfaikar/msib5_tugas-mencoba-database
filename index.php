dcaebbc965aa620d55a09d71402d60584982a4e8
farisfaikar <farisfaikar.r@gmail.com>
2023-09-08 13:42:35 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// Memanggil koneksi menuju database
+include_once("connection.php");
+
+// Memanggil data dari database
+// $result = mysqli_query($mysqli, 'SELECT * FROM users');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>MSIB 5 - Tugas Mencoba Database</title>
+</head>
+<body class="p-2 bg-zinc-900">
+    <!-- Card -->
+    <div class="bg-zinc-800 text-zinc-100 rounded">
+        <h1>MSIB 5 - Tugas Mencoba Database</h1>
+    </div>
+</body>
+</html>
